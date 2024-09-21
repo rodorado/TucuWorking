ba71c09f0f76071667d434f285aa6de7d56d74ef
Rocio Dorado <rocio_dorado12@hotmail.com>
2024-09-21 03:55:11 -0300

    Schema salas creado, falta usuarios y reservas

---
+++ db/config.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const mongoose = require("mongoose");
+
+try {
+  mongoose.connect(process.env.MONGODB_CONNECT).then(() => console.log('DB Conectada'))
+
+} catch (error) {
+  console.log(error);
+}
+
+module.exports = mongoose
--- server/config.js
+++ server/config.js
@@ -1,3 +1,4 @@
+require('../db/config')
 const express = require("express");
 const path = require("path");
 const cors = require("cors")
