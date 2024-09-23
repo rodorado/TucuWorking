9c1d1a9e54a1e5cd13145369b61de9797cbea0e2
Rocio Dorado <rocio_dorado12@hotmail.com>
2024-09-23 00:01:08 -0300

    Validaciones completas, Roles y JWT, falta reservas

--- routes/salas.routes.js
+++ routes/salas.routes.js
@@ -1,21 +1,23 @@
 const express = require("express");
 const { obtenerUnaSalaPorIdOTodos, crearUnaSala, editarUnaSala, borrarUnaSala, habilitarSala,deshabilitarUnaSala } = require("../controllers/salas.controllers");
-const { agregarSalaValidaciones } = require("../middlewares/validaciones");
+const { agregarSalaValidaciones} = require("../middlewares/validaciones");
+const auth = require('../middlewares/auth')
+
 const router = express.Router();
 
 //GET
 router.get("/:idSala?", obtenerUnaSalaPorIdOTodos);
 
 //POST
-router.post("/", agregarSalaValidaciones, crearUnaSala);
+router.post("/", agregarSalaValidaciones, auth('admin'), crearUnaSala);
 
 //PUT
-router.put("/:idSala", agregarSalaValidaciones, editarUnaSala);
+router.put("/:idSala", auth('admin'), agregarSalaValidaciones, editarUnaSala);
 
 //DELETE
-router.delete("/:idSala", borrarUnaSala);
+router.delete("/:idSala", auth('admin'), borrarUnaSala);
 
-router.put('/habilitar/:idSala', habilitarSala)
-router.put('/deshabilitar/:idSala', deshabilitarUnaSala)
+router.put('/habilitar/:idSala', auth('admin'), habilitarSala)
+router.put('/deshabilitar/:idSala', auth('admin'), deshabilitarUnaSala)
 
 module.exports = router;
--- services/usuarios.services.js
+++ services/usuarios.services.js
@@ -1,11 +1,29 @@
 const usuarioModel = require("../models/usuarios.schemas");
 const bcrypt = require("bcrypt")
+const jwt = require('jsonwebtoken')
 
-
-const traerTodosLosUsuarios = async() => {
+const traerTodosLosUsuarios = async(limit, to, verBloqueados) => {
   try {
-    const obtenerUsers = await usuarioModel.find()
-    return obtenerUsers
+    /*const obtenerUsers = await usuarioModel.find()
+    return obtenerUsers*/
+    try {
+      const query = verBloqueados ? {} : { bloqueado: true }; // Si se quiere ver bloqueados, no se aplica el filtro
+  
+      const [usuarios, cantidadTotal] = await Promise.all([
+        usuarioModel.find(query).skip(to * limit).limit(limit),
+        usuarioModel.countDocuments(verBloqueados ? {} : { bloqueado: true })
+      ]);
+  
+      const paginacion = {
+        usuarios,
+        cantidadTotal
+      };
+      return paginacion;
+
+    } catch (error) {
+      console.log(error);
+    }
+
   } catch (error) {
     console.log(error);
   }
@@ -19,13 +37,21 @@ const traerUnUsuario = async(id) => {
     console.log(error);
   }
 };
+
 const añadirUnUsuario = async (body) => {
   try {
-   
-    const usuarioExiste = await usuarioModel.findOne({ email: body.email })
+    const usuarioExiste = await usuarioModel.findOne({ email: body.email });
 
     if (usuarioExiste) {
       return { error: true, msg: "Error al registrar usuario", detalle: error };
+    }
+
+    if (!body.rol) {
+      body.rol = 'usuario';
+    }
+
+    if (body.rol !== 'usuario' && body.rol !== 'admin') {
+      return 409;
     }
 
     let salt = bcrypt.genSaltSync();
@@ -51,7 +77,15 @@ const inicioSesion = async (body) => {
     const verificacionContrasenia = bcrypt.compareSync(body.contrasenia, usuarioExiste.contrasenia);
 
     if (verificacionContrasenia) {
-      return { code: 200, msg: "Inicio de sesión exitoso", usuario: usuarioExiste };
+      const payload = {
+        _id: usuarioExiste._id,
+        rol: usuarioExiste.rol,
+        bloqueado: usuarioExiste.bloqueado,
+      }
+
+      const token = jwt.sign(payload, process.env.JWT_SECRET)
+
+      return { code: 200, token, msg: "Inicio de sesión exitoso", usuario: usuarioExiste };
     } else {
       return { code: 400, msg: "Contraseña incorrecta" };
     }
@@ -64,15 +98,36 @@ const inicioSesion = async (body) => {
 
 const modificarUsuario = async (idUsuario, data) => {
   try {
-    const salt = await bcrypt.genSalt(10);
-    data.contrasenia = await bcrypt.hash(data.contrasenia, salt);
-    const usuario = await usuarioModel.findByIdAndUpdate({_id: idUsuario}, data, {new: true});
-    return usuario
+    const usuario = await usuarioModel.findById(idUsuario);
+
+    if (!usuario) {
+      return { error: true, msg: 'Usuario no encontrado' };
+    }
+
+    // Verifica si el rol es "usuario" y no permite cambiar a "admin"
+    if (usuario.rol === 'usuario' && data.rol === 'admin') {
+      return { error: true, msg: "Un usuario no puede cambiar su rol a admin." };
+    }
+
+    // Eliminar el campo "bloqueado" si existe en la data
+    delete data.bloqueado;
+
+    // Encriptar la nueva contraseña si está presente
+    if (data.contrasenia) {
+      const salt = await bcrypt.genSalt(10);
+      data.contrasenia = await bcrypt.hash(data.contrasenia, salt);
+    }
+
+    // Actualizar el usuario
+    const usuarioActualizado = await usuarioModel.findByIdAndUpdate(idUsuario, data, { new: true });
+    return { error: false, usuario: usuarioActualizado };
   } catch (error) {
     console.log(error);
     return { error: true, msg: "Error al actualizar usuario", detalle: error.message };
   }
 };
+
+
 
 
 const borradoFisicoUsuario = async(idUsuario) => {
